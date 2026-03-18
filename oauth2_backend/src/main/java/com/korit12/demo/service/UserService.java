6991fd0de297f14708aca0c2b95cfa0fa13b8e8b
maybeags <suren0709@gmail.com>
2026-03-18 17:50:00 +0900

    feat: oauth2 backend making

--- oauth2_backend/src/main/java/com/korit12/demo/service/UserService.java
+++ oauth2_backend/src/main/java/com/korit12/demo/service/UserService.java
@@ -1,6 +1,7 @@
 package com.korit12.demo.service;
 
 import com.korit12.demo.dto.AuthResponseDto;
+import com.korit12.demo.dto.LoginRequestDto;
 import com.korit12.demo.dto.SignupRequestDto;
 import com.korit12.demo.entity.User;
 import com.korit12.demo.repository.UserRepository;
@@ -29,6 +30,24 @@ public class UserService {
 
         // User field 자체에는 token이 없고 ResponseDto에서 붙여서 보낼겁니다.
         String token = jwtService.generateToken(user.getEmail(), user.getRole().name());
+        return AuthResponseDto.of(token, user.getEmail(), user.getName(), user.getRole().name());
     }
 
+    public AuthResponseDto login(LoginRequestDto dto) {
+        User user = userRepository.findByEmail(dto.getEmail())
+                .orElseThrow(() -> new IllegalArgumentException("email 혹은 비밀번호가 잘못되었습니다."));
+
+        // OAuth2 적용인지 판단하겠습니다.
+        if (user.getPassword() == null) {
+            throw new IllegalArgumentException("소셜 로그인 계정입니다. 구글/네이버/카카오 로그인을 이용해주세요.");
+        }
+
+        // BCrypt로 비밀번호 검증
+        if(!passwordEncoder.matches(dto.getPassword(), user.getPassword())) { // if(dto.getPassword() == user.getPassword()) {}
+            throw new IllegalArgumentException("email 또는 비밀번호가 잘못되었습니다.");
+        }
+
+        String token = jwtService.generateToken(user.getEmail(), user.getRole().name());
+        return AuthResponseDto.of(token, user.getEmail(), user.getName(), user.getRole().name());
+    }
 }
